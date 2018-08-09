cc79acec0434dbe340bce8053833cfcab7f4dfd8
maruleba <folaa1234@gmail.com>
2018-08-09 17:23:05 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,13 @@
 #write out your code here
 
-def least_coins(cents)
-
+def least_coins(cents) 
+least_coins = 29
+coins { "quarters","dimes","nickels","dollars"}
+values  { 5,1,3,4}
+puts coins 
 #Code your answer here!
 
 end
+
+least_coins do |money|
+  
